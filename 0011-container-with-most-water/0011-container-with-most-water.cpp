f43e3ffa90ad4c7b8f4a44b68b8c8aa73adc42ff
Anurag Singh <anuragh3re@gmail.com>
2026-07-11 11:27:27 +0530

    Time: 7 ms (6.74%), Space: 62.9 MB (48.93%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,15 +5,13 @@ public:
         int l = 0;
         int r=nums.size()-1;
         while (l<r){
+            maxArea = max(maxArea,min(nums[l],nums[r])*(r-l));
             if(nums[l]<nums[r]){
-                maxArea = max(maxArea,min(nums[l],nums[r])*(r-l));
                 l++;
             }else{
-                maxArea = max(maxArea,min(nums[l],nums[r])*(r-l));
                 r--;
             }
         }
-        
         return maxArea;
     }
 };
