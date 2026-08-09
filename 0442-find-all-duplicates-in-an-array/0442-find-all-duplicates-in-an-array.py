1f9574c7e6cc49608d3cb49248e131944b634caf
Anurag Singh <anuragh3re@gmail.com>
2026-08-10 00:25:20 +0530

    Time: 34 ms (48.99%), Space: 35.4 MB (31.31%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,17 +1,5 @@
-class Solution(object):
-    def findDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        # seen = set()
-        # duplicates = []
-        # for i in nums:
-        #     if i not in seen:
-        #         seen.add(i)
-        #     else:
-        #         duplicates.append(i)
-        # return duplicates
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
         freq = {}
         res = []
         for i in nums:
@@ -23,7 +11,3 @@ class Solution(object):
             if freq[i]>1:
                 res.append(i)
         return res
-
-
-
-        
